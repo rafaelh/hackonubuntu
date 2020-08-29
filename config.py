1ba57bf3188631580eb429a39dd3cdb3a4cb5b62
Rafe Hart <rafael.hart9@gmail.com>
2020-08-29 16:48:03 +1000

    Setup changes

--- config.py
+++ config.py
@@ -5,20 +5,24 @@ import os
 # TODO:
 # amass
 
+# === Stuff to Remove ===
 
 # These directories will be removed from your home directory
 directories_to_remove = ['Documents', 'Music', 'Pictures', 'Public', 'Templates', 'Videos']
+
+# These APT packages will be removed
+packages_to_remove = []
+
+
+# === Stuff to Add ===
 
 # These kali packages will be installed
 packages_to_install = ['most', 'ttf-mscorefonts-installer', 'pydf', 'htop', 'golang', 'exif', 'hexedit', 'jq', 'python3-pip', 
                        'python3-venv', 'apt-transport-https', 'curl', 'filezilla', 'meld',
                        'net-tools', 'tmux']
 
-# These kali packages will be removed
-packages_to_remove = ['zsh', 'zsh-syntax-highlighting', 'zsh-autosuggestions']
-
 # These python packages will be installed globally
-pip_packages = ['pipenv', 'pylint', 'dnsgen']
+pip_packages = ['pipenv', 'pylint']
 
 # These go tools will be installed globally. You will need to have the following settings in your
 # .bashrc already:
@@ -38,10 +42,7 @@ golang_modules_to_install = [
 external_tools_directory = '/opt'
 ext_repositories_to_sync = [
                             'https://github.com/danielmiessler/SecLists',
-                            'https://github.com/swisskyrepo/PayloadsAllTheThings',
-                            'https://github.com/payloadbox/xss-payload-list',
-                            'https://github.com/Cillian-Collins/dirscraper',
-                            'https://github.com/maurosoria/dirsearch'
+                            'https://github.com/swisskyrepo/PayloadsAllTheThings'
                             ]
 
 # These git repositories will be synced to the 'personal repo' directory. I use my home directory.
